4eda1776577fbc171e35391c95f9aa6942a91062
Kroquetitaa <Kroquetita@gmail.com>
2022-09-09 17:39:09 +0200

    booleans

---
+++ dist/03-booleans.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    let isEnable = true;
+    console.log('isEnable', isEnable);
+    isEnable = false;
+    console.log('isEnable', isEnable);
+    const random = Math.random();
+    console.log('random', random);
+    isEnable = random >= 0.5 ? true : false;
+    console.log('isEnable', isEnable);
+})();
