91bc917a7a5ad1c1f600ce178aca6c6672fb68cb
Kroquetitaa <Kroquetita@gmail.com>
2022-09-09 17:49:01 +0200

    arrays

---
+++ dist/05-arrays.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    let prices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    console.log(prices);
+    let mixed = ['hola', true];
+    mixed.push(12);
+    mixed.push('asd');
+    mixed.push(true);
+    mixed.push({});
+    mixed.push([]);
+    console.log(mixed);
+})();
