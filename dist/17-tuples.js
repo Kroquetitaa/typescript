f215060f026a4779c0d2ec3e170d1a9fab1f01e9
Kroquetitaa <Kroquetita@gmail.com>
2022-09-11 18:10:44 +0200

    tuples

---
+++ dist/17-tuples.js
@@ -0,0 +1,6 @@
+"use strict";
+const prices = [1, 2, 3, 4, 5, 'as'];
+let user;
+user = ['nicobytes', 15, true];
+const [username, age] = user;
+console.log(username, age);
---
+++ src/17-tuples.ts
@@ -0,0 +1,6 @@
+const prices: (number | string)[] = [1,2,3,4,5,'as'];
+
+let user: [string, number, boolean];
+user = ['nicobytes', 15, true];
+const [ username, age] = user;
+console.log( username, age);
